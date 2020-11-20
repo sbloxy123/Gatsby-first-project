0db020cceb8d19085bbaf7c39650ca05c861ea13
Stuart Bloxham <s.bloxham@hotmail.com>
2020-11-20 20:04:58 +0000

    updated .env file name

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -2,7 +2,6 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "gatsby-image"
-import layout from "../components/layout"
 
 const ComponentName = ({
   data: {
